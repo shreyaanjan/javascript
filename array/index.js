68e09a8550adbdfb0196d0754d07a5bd4f4f4e8a
shreyaanjan <shreyaanjan02@gmail.com>
2025-02-19 18:50:09 +0530

    first commit for js learnings

---
+++ array/index.js
@@ -0,0 +1,42 @@
+// simple for loop
+// let arr = [10, 20, 30, 40, 50];
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+//for of loop
+// let fruits = ["Apple", "Mango", "Kiwi"];
+// for(let value of fruits){
+//     console.log(value);
+// }
+
+// for in loop
+// let food = ["ice-cream", "pizza", "pasta", "pani-puri"];
+// for(let type in food){
+//     console.log(type, food[type]);
+// }
+
+// object iteration
+// let info = {
+//     name: "Shreya Anjan",
+//     grid: 2222,
+// }
+// console.log(info);
+// console.log(info.grid, info.name);
+
+// info.name = "Janvi Patel";
+// info.grid = 3333;
+// info.age = 15;
+// console.log(info);
+
+// for(let key in info){
+//     console.log(key, info[key]);
+// }
+
+let arr = [10, 20, 30, 40];
+// arr[4] = [60, 30, 80, 100];
+// arr[arr.length] = 100, 200;
+// console.log(arr);
+
+arr.push(100, 200, 300);
+console.log(arr);
