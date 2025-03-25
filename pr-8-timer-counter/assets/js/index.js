ac6d0ab11e8cc034fd7b5bd64b5d98464054649b
shreyaanjan <shreyaanjan02@gmail.com>
2025-03-25 18:25:14 +0530

    done local-storage crud and made some quote changes in timer

--- pr-8-timer-counter/assets/js/index.js
+++ pr-8-timer-counter/assets/js/index.js
@@ -5,6 +5,21 @@ let runningState = false;
 let confettiRunning;
 let newFlag = false;
 let paused = false;
+let newQuote = document.getElementById("newQuote")
+let newAuthor = document.getElementById("newAuthor")
+
+let quotes = [
+    { quote: "All that glitters is not gold.", author: "- William Shakespeare" },
+    { quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.", author: "- Albert Einstein" },
+    { quote: "No one can make you feel inferior without your consent.", author: "- Eleanor Roosevelt" },
+    { quote: "Whatever you are, be a good one.", author: "- Abraham Lincoln" },
+    { quote: "Be yourself; everyone else is already taken.", author: "- Oscar Wilde" },
+    { quote: "Our greatest glory is not in never falling, but in rising every time we fall.", author: "- Nelson Mandela" },
+    { quote: "You don’t have to be great to start, but you have to start to be great.", author: "- Zig Ziglar" },
+    { quote: "It is during our darkest moments that we must focus to see the light.", author: "- Aristotle" },
+    { quote: "You are never too old to set another goal or to dream a new dream.", author: "- C.S. Lewis" },
+    { quote: "To live is the rarest thing in the world. Most people just exist.", author: "- Oscar Wilde" }
+]
 
 document.getElementById("start").addEventListener("click", () => {
     if (runningState) return;
@@ -95,6 +110,10 @@ function updateTime() {
 function showModal() {
     const myModal = new bootstrap.Modal(document.getElementById('quoteModal'));
     myModal.show();
+    let val = parseInt(Math.random() * 10)
+    newQuote.innerHTML = quotes[val].quote;
+    newAuthor.innerHTML = quotes[val].author;
+
     let audio = document.getElementById("sound");
 
     confettiRunning = setInterval(() => {
