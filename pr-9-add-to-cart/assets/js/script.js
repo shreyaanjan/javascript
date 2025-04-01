df42a6a8a0b7b1ad0ee4ec325507c3ee8d3c898d
shreyaanjan <shreyaanjan02@gmail.com>
2025-04-01 17:15:20 +0530

    made some sections

---
+++ pr-9-add-to-cart/assets/js/script.js
@@ -0,0 +1,44 @@
+let products = [
+    {
+        id: 1,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-1.webp",
+        name: "Fresh Red Tomatos",
+        price: 6.99,
+    },
+    {
+        id: 2,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-2.webp",
+        name: "Fresh Mashroom",
+        price: 5.99,
+    },
+    {
+        id: 3,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-3.webp",
+        name: "Fresh Watermelon",
+        price: 6.99,
+    },
+    {
+        id: 4,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-4.webp",
+        name: "White Eggplant",
+        price: 6.99,
+    },
+    {
+        id: 5,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-5.webp",
+        name: "White Eggplant",
+        price: 6.99,
+    },
+    {
+        id: 6,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-6.webp",
+        name: "White Eggplant",
+        price: 6.99,
+    },
+]
+
+products.forEach((products, idx)=>{
+    document.getElementById("rowContent").innerHTML += `
+        
+    `
+})
