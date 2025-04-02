0ad83477344b61745c940f2d27fb623164b41e2b
shreyaanjan <shreyaanjan02@gmail.com>
2025-04-02 16:59:11 +0530

    design and half code done

--- pr-9-add-to-cart/assets/js/script.js
+++ pr-9-add-to-cart/assets/js/script.js
@@ -7,8 +7,8 @@ let products = [
     },
     {
         id: 2,
-        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-2.webp",
-        name: "Fresh Mashroom",
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-15.webp",
+        name: "Fresh Kiwi",
         price: 5.99,
     },
     {
@@ -19,26 +19,71 @@ let products = [
     },
     {
         id: 4,
-        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-4.webp",
-        name: "White Eggplant",
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-20.webp",
+        name: "Fresh Potato",
         price: 6.99,
     },
     {
         id: 5,
-        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-5.webp",
-        name: "White Eggplant",
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-22.webp",
+        name: "Fresh Cookies",
         price: 6.99,
     },
     {
         id: 6,
-        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-6.webp",
-        name: "White Eggplant",
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-24.webp",
+        name: "Fresh Beetroot",
+        price: 6.99,
+    },
+    {
+        id: 7,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-21.webp",
+        name: "Fresh Oranges",
+        price: 6.99,
+    },
+    {
+        id: 8,
+        image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-16.webp",
+        name: "Fresh Broccoli",
         price: 6.99,
     },
 ]
 
-products.forEach((products, idx)=>{
+let cartArr = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(productId){
+    let product = cartArr.find((obj) => obj.id === productId);
+
+    if(product){
+        product.quantity++;
+    } else {
+        let productToAdd = products.find((obj) => obj.id === productId);
+
+        if(productToAdd){
+            productToAdd.quantity = 1;
+            cartArr.push(productToAdd);
+        }
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cartArr));
+   
+}
+
+products.forEach((product, idx) => {
     document.getElementById("rowContent").innerHTML += `
-        
+        <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+            <div class="product-wrapper text-center">
+                <div class="product-img">
+                    <img src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="product-info py-3">
+                    <h3>${product.name}</h3>
+                    <span class="text-danger">${product.price}</span>
+                </div>
+                <div class="add-cart">
+                    <button class="btn btn-success add-btn w-100" onclick="addToCart(${product.id})">Add To Cart</button>
+                </div>
+            </div>
+        </div>               
     `
 })
