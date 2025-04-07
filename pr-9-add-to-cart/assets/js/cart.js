2ab33c849ec77eb2add971e47b7da0bc17a28f91
shreyaanjan <shreyaanjan02@gmail.com>
2025-04-07 16:51:14 +0530

    cart js done

---
+++ pr-9-add-to-cart/assets/js/cart.js
@@ -0,0 +1,91 @@
+let cartArr = JSON.parse(localStorage.getItem("cart")) || [];
+let counter = document.getElementById("counter");
+counter.innerHTML = cartArr.length;
+let cartItems = document.getElementById("cartItems");
+let totalAmount = 0;
+let shopTitle = document.querySelector(".shop-title");
+
+const saveArr = ()=>{
+    localStorage.setItem("cart",JSON.stringify(cartArr))
+}
+
+function deleteItem(idx){
+    cartArr.splice(idx, 1);
+    saveArr();
+    displayCart()
+
+}
+
+function updateQuantity(idx, value){
+    let newQuantity = cartArr[idx].quantity + value;
+
+    if(newQuantity <= 0){
+        deleteItem(idx);
+    } else {
+        cartArr[idx].quantity = newQuantity;
+        saveArr();
+    }
+    displayCart();
+}
+
+const displayCart = ()=>{
+    totalAmount = 0;
+    cartItems.innerHTML = "";
+
+    if(cartArr.length === 0){
+        cartItems.innerHTML = `
+            <div>
+                <img class="img-fluid" src="https://static.vecteezy.com/system/resources/previews/016/462/240/non_2x/empty-shopping-cart-illustration-concept-on-white-background-vector.jpg" alt="emptycart">
+            </div>
+        `
+        shopTitle.classList.add("d-none");
+    } else {
+        shopTitle.classList.remove("d-none");
+    }
+
+    cartArr.forEach((shopItem, idx)=>{
+        let subTotal = shopItem.quantity * shopItem.price;
+        totalAmount += subTotal
+        
+        cartItems.innerHTML += `
+        
+            <div class="col-6 mt-3">
+                <div class="d-flex gap-4 align-items-center">
+                    <div class="product-image">
+                        <img width="100%" class="border" src="${shopItem.image}" 
+                            alt="">
+                    </div>
+                    <div class=" ">
+                        ${shopItem.name}
+                    </div>
+                </div>
+            </div> 
+            <div class="col-2 ">
+                <div class="">
+                    <h5>$${shopItem.price}</h5>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="d-flex justify-content-between border quantity">
+                    <button class="border-0" onclick="updateQuantity(${idx}, -1)">
+                        <i class="bi bi-dash"></i>
+                    </button>
+                    <span>${shopItem.quantity}</span>
+                    <button class="border-0" onclick="updateQuantity(${idx}, 1)">
+                        <i class="bi bi-plus"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="d-flex align-items-center gap-5">
+                    <span>$${subTotal}</span>
+                    <button class="border-0" onclick="deleteItem(${idx})">
+                        <i class="bi bi-x"></i>
+                    </button>
+                </div>
+            </div>
+        `
+    })
+    
+}
+displayCart();
--- pr-9-add-to-cart/assets/js/script.js
+++ pr-9-add-to-cart/assets/js/script.js
@@ -3,49 +3,49 @@ let products = [
         id: 1,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-1.webp",
         name: "Fresh Red Tomatos",
-        price: 6.99,
+        price: 100,
     },
     {
         id: 2,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-15.webp",
         name: "Fresh Kiwi",
-        price: 5.99,
+        price: 200,
     },
     {
         id: 3,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-3.webp",
         name: "Fresh Watermelon",
-        price: 6.99,
+        price: 150,
     },
     {
         id: 4,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-20.webp",
         name: "Fresh Potato",
-        price: 6.99,
+        price: 250,
     },
     {
         id: 5,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-22.webp",
         name: "Fresh Cookies",
-        price: 6.99,
+        price: 300,
     },
     {
         id: 6,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-24.webp",
         name: "Fresh Beetroot",
-        price: 6.99,
+        price: 150,
     },
     {
         id: 7,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-21.webp",
         name: "Fresh Oranges",
-        price: 6.99,
+        price: 100,
     },
     {
         id: 8,
         image: "https://quomodosoft.com/html/ecoshop/assets/images/homepage-one/product-img/p-img-16.webp",
         name: "Fresh Broccoli",
-        price: 6.99,
+        price: 100,
     },
 ]
 
@@ -53,15 +53,20 @@ let cartArr = JSON.parse(localStorage.getItem("cart")) || [];
 let counter = document.getElementById("counter");
 counter.innerHTML = cartArr.length;
 
-function addToCart(productId){
+function addToCart(productId) {
     let product = cartArr.find((obj) => obj.id === productId);
 
-    if(product){
-        product.quantity++;
+    if (product) {
+        // product.quantity++;
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Item added once !",
+        });
     } else {
         let productToAdd = products.find((obj) => obj.id === productId);
 
-        if(productToAdd){
+        if (productToAdd) {
             productToAdd.quantity = 1;
             cartArr.push(productToAdd);
         }
@@ -80,7 +85,7 @@ products.forEach((product, idx) => {
                 </div>
                 <div class="product-info py-3">
                     <h3>${product.name}</h3>
-                    <span class="text-danger">${product.price}</span>
+                    <span class="text-danger">$${product.price}</span>
                 </div>
                 <div class="add-cart">
                     <button class="btn btn-success add-btn w-100" onclick="addToCart(${product.id})">Add To Cart</button>
