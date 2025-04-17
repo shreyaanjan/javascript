cdc42b323c65aa0a60c82139e6de70d9b20cddce
shreyaanjan <shreyaanjan02@gmail.com>
2025-04-17 16:40:46 +0530

    all done

--- pr-11-corona-api/assets/js/script.js
+++ pr-11-corona-api/assets/js/script.js
@@ -1,9 +1,15 @@
-let ans = document.getElementById("ans");
+let result = document.getElementById("result");
+let con = document.getElementById("confirm");
+let recover = document.getElementById("recover");
+let death = document.getElementById("death");
+let loc = document.getElementById("location");
 
 document.getElementById("check").addEventListener("click", () => {
     let ctName = document.getElementById("ctName").value;
 
     const URL = `https://api.rootnet.in/covid19-in/stats/latest`;
+
+    document.getElementById("ctName").value = "";
 
     fetch(URL)
         .then((res) => {
@@ -11,18 +17,29 @@ document.getElementById("check").addEventListener("click", () => {
         })
         .then((res) => {
             let states = res.data.regional;
+            let found = false;
             console.log(states);
 
             for (let i = 0; i < states.length; i++) {
                 if (states[i].loc.toLowerCase() === ctName.toLowerCase()) {
-                    console.log(states[i]);
-
-                    ans.innerHTML +=
-                        "Total Confirmed : " + states[i].totalConfirmed + "<br>" +
-                        "Deaths : " + states[i].deaths + "<br>"
-                    return;
+                    loc.innerHTML = "Location : " + states[i].loc;
+                    con.innerHTML = "Total Confirmed : " + states[i].totalConfirmed;
+                    death.innerHTML = "Deaths : " + states[i].deaths;
+                    recover.innerHTML = "Recovered : " + states[i].discharged;
+                    found = true;
+                    break;
                 }
             }
-            ans.innerHTML = `State not Found. Please Check the Spelling.`
+            
+            if (!found) {
+                loc.innerHTML = `Location : State not Found !`;
+                con.classList.add("d-none");
+                death.classList.add("d-none");
+                recover.classList.add("d-none");
+            }else {
+                con.classList.remove("d-none");
+                death.classList.remove("d-none");
+                recover.classList.remove("d-none");
+            }
         })
 })
